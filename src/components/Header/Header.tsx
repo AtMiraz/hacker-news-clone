fd6e93c92786764248d9f71d683b83c7ee50dce8
Jose Antonio <antonio.miranda9696@gmail.com>
2023-05-18 18:52:13 -0600

    Adds Header feature, moves normalize.css to css folder

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import './Header.css'
+import logo from '../../assets/svgs/hacker-news.svg'
+
+function Header() {
+  return (
+    <>
+      <div className="hacker__news__app-header">
+        <h1> Hacker News </h1>
+        <img src={logo} alt="logo" />
+      </div>
+    </>
+  )
+}
+
+export default Header;
